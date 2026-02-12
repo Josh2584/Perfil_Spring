70e47e067933f7513ece18e590926489de3f25fc
Josh2584 <joshuaporras144@gmail.com>
2026-02-12 17:42:45 -0600

    Estructura del proyecto

---
+++ Perfil/src/test/java/com/example/perf/PerfilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
