ec8224113331aab193ac00781655d76727064b18
Josh2584 <joshuaporras144@gmail.com>
2026-02-12 19:37:35 -0600

    Pagina funcional
    Se agregan las dependencias y códigos que no necesitan ser alterados en primera instancia y luego los códigos desarrollados

---
+++ main/java/com/example/perf/PerfilApplication.java
@@ -0,0 +1,13 @@
+package com.example.perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilApplication.class, args);
+	}
+
+}
